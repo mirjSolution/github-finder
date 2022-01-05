4e0fe2be03a0aa37399336ccbb0707bd5a3c3a1b
Rico John Dato-on <ricojohndatoon23@gmail.com>
2022-01-04 22:31:15 -0500

    Clear users

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -47,6 +47,9 @@ export const GithubProvider = ({ children }) => {
     dispatch({ type: 'GET_USERS', payload: items });
   };
 
+  // Clear users from state
+  const clearUsers = () => dispatch({ type: 'CLEAR_USERS' });
+
   // Set loading
   const setLoading = () => {
     dispatch({ type: 'SET_LOADING' });
@@ -54,7 +57,12 @@ export const GithubProvider = ({ children }) => {
 
   return (
     <GithubContext.Provider
-      value={{ users: state.users, loading: state.loading, searchUsers }}
+      value={{
+        users: state.users,
+        loading: state.loading,
+        searchUsers,
+        clearUsers,
+      }}
     >
       {children}
     </GithubContext.Provider>
