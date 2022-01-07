36e21aeb008559330cedb35b0b4492603067d220
Rico John Dato-on <ricojohndatoon23@gmail.com>
2022-01-06 19:59:08 -0500

    Refactor, Action File and using axios

--- src/components/pages/User.jsx
+++ src/components/pages/User.jsx
@@ -3,20 +3,24 @@ import { useEffect, useContext } from 'react';
 import { useParams } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import Spinner from '../layout/Spinner';
-import GithubContext from '../../context/github/GithubContext';
 import RepoList from '../repos/RepoList';
+import GithubContext from '../../context/github/GithubContext';
+import { getUserAndRepos } from '../../context/github/GithubActions';
 
 function User() {
-  const { getUser, user, loading, getUserRepos, repos } =
-    useContext(GithubContext);
+  const { user, loading, repos, dispatch } = useContext(GithubContext);
 
   const params = useParams();
 
   useEffect(() => {
-    getUser(params.login);
-    getUserRepos(params.login);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+    dispatch({ typw: 'SET_LOADING' });
+    const getUserData = async () => {
+      const userData = await getUserAndRepos(params.login);
+      dispatch({ type: 'GET_USER_AND_REPOS', payload: userData });
+    };
+
+    getUserData();
+  }, [dispatch, params.login]);
 
   const {
     name,
